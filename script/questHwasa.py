069d3c964be9e83b12ac242f8d7c743b70ae2eb2
yifyu1122 <yifyu1122@gmail.com>
2025-03-12 17:41:33 +0800

    好累ㄚㄚㄚㄚㄚㄚㄚ

--- script/questHwasa.py
+++ script/questHwasa.py
@@ -5,9 +5,10 @@ class questHwasa:
        self.gossip = 0
        
     def quest(self):
+        #我們的祖先曾經有過翅膀，住在空中的島嶼。隨著汙染的擴散，我們的翅膀已經退化了。
         print("\n🌟 **華莎**：「你好，冒險者！我有一個任務需要你的幫助。」")
         if not village.unlocked_features["quest"]:
             village.unlocked_features["quest"] = True
-            print("✨ 你解鎖了 **任務系統**，可以接受華薩的任務！")
+            print("✨ 你解鎖了 **任務系統**，可以接受華莎的任務！")
         else:
             print("🔍 你可以向華莎接受任務，完成任務可以獲得獎勵。")
